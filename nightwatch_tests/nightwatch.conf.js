cc70ef776a44a1cba2f7d5b9805e745e589d1d9d
alanrtao <alanrtao@outlook.com>
2022-11-04 17:22:47 -0400

    initialize nightwatch

--- nightwatch_tests/nightwatch.conf.js
+++ nightwatch_tests/nightwatch.conf.js
@@ -14,7 +14,7 @@
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ['tests','nightwatch/examples'],
+  src_folders: ['tests'],
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
   page_objects_path: ['nightwatch/page-objects'],
